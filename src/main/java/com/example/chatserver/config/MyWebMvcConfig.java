b483647a876138f333a92e5c95df48b4548a9dcc
a240506 <1187668245@qq.com>
2022-09-17 22:39:39 +0800

    可以请求服务器了

---
+++ src/main/java/com/example/chatserver/config/MyWebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.chatserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+文件创建于  2022/09/17  17：06
+*/
+//配置跨域
+@Configuration
+public class MyWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("*");
+    }
+}
--- src/main/java/com/example/chatserver/serve/LoginServe.java
+++ src/main/java/com/example/chatserver/serve/LoginServe.java
@@ -1,34 +1,31 @@
-package com.example.chatserver.ui;
-
+package com.example.chatserver.serve;
 
 import com.example.chatserver.common.R;
 import com.example.chatserver.common.Tool;
 import com.example.chatserver.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /*
-文件创建于  2022/09/17  13：01
+文件创建于  2022/09/17  17：01
 */
 @RestController
-public class a {
+public class LoginServe {
+
     @Autowired
     private UserServiceImpl userService;
 
-    @GetMapping(value = "/toChatroom")
-    public String toChatroom() {
-        return "chat";
-    }
-
-    @RequestMapping( value = "/goods",method = RequestMethod.POST)
+    @RequestMapping( value = "/login",method = RequestMethod.POST)
     public R getRoles(@RequestBody Map<String, Object> params){
         Map<String,Object> meta = new HashMap<String,Object>();
 
         meta.put("a",userService.loadByName("test"));
-
 
         return Tool.result(meta,200,"获取成功");
     }
